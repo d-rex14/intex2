912e950bde60aeb74b8449da3df8776fc1e324af
Joshua Schmitt <schmtty@byu.edu>
2026-04-09 13:26:07 -0600

    Fixed images

--- src/content/board.ts
+++ src/content/board.ts
@@ -1,7 +1,7 @@
-import grahamImg from 'C:/Users/jrsch/.cursor/projects/c-Users-jrsch-OneDrive-Desktop-IS-Core-INTEX-3-0-intex2/assets/c__Users_jrsch_AppData_Roaming_Cursor_User_workspaceStorage_1a96fc19e0218a699355f274f5f5d3b9_images_Headshot_-_Copy-950477d5-a6e9-4480-9100-d610b16690d7.png'
-import blaneImg from 'C:/Users/jrsch/.cursor/projects/c-Users-jrsch-OneDrive-Desktop-IS-Core-INTEX-3-0-intex2/assets/c__Users_jrsch_AppData_Roaming_Cursor_User_workspaceStorage_1a96fc19e0218a699355f274f5f5d3b9_images_IMG_7252-1b3611cc-50b2-4a95-821a-438aabff5515.png'
-import devinImg from 'C:/Users/jrsch/.cursor/projects/c-Users-jrsch-OneDrive-Desktop-IS-Core-INTEX-3-0-intex2/assets/c__Users_jrsch_AppData_Roaming_Cursor_User_workspaceStorage_1a96fc19e0218a699355f274f5f5d3b9_images_IMG_1250-75a853c0-8b9a-41ed-b0f1-f33445e75580.png'
-import joshImg from 'C:/Users/jrsch/.cursor/projects/c-Users-jrsch-OneDrive-Desktop-IS-Core-INTEX-3-0-intex2/assets/c__Users_jrsch_AppData_Roaming_Cursor_User_workspaceStorage_1a96fc19e0218a699355f274f5f5d3b9_images_Schmitt__Joshua__1_-c5de3dc8-eecf-41ac-939f-a419df549755.png'
+import grahamImg from '../assets/board/graham-fawson.jpg'
+import blaneImg from '../assets/board/blane-santilli.jpg'
+import devinImg from '../assets/board/devin-holderness.jpg'
+import joshImg from '../assets/board/joshua-schmitt.jpg'
 
 export const BOARD = [
   {
